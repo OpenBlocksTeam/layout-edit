ac95959275d8390d9f833b09fa097ad60e021715
Iyxan23 <nurihsanalghifari@gmail.com>
2021-03-13 10:23:50 +0700

    add: Implement LVerticalLinearLayout

--- lib/src/main/java/com/openblocks/module/layoutedit/widgets/LHorizontalLinearLayout.java
+++ lib/src/main/java/com/openblocks/module/layoutedit/widgets/LHorizontalLinearLayout.java
@@ -14,7 +14,53 @@ public class LHorizontalLinearLayout extends LVerticalLinearLayout {
     }
 
     @Override
-    public void draw(Canvas canvas, int x, int y, int height, int width) {
-        drawOutline(canvas, x, y, height, width);
+    public int getMinWidth() {
+        int width_total = margin.left + padding.left;
+
+        for (LWidget child : childs) {
+            width_total +=
+                    child.margin.left + child.padding.left +
+                    child.getWidth() +
+                    child.margin.right + child.padding.right;
+        }
+
+        width_total += margin.right + padding.right;
+
+        return width_total;
+    }
+
+    @Override
+    public int getMinHeight() {
+        int height_total =
+                margin.top + padding.top
+                 + margin.bottom + padding.bottom;
+
+        for (LWidget child : childs) {
+            height_total =
+                    Math.max(
+                            child.margin.top + child.padding.top +
+                            child.getHeight() +
+                            child.margin.bottom + child.padding.bottom
+                            ,
+                            height_total
+                    );
+        }
+
+        height_total += margin.bottom + padding.bottom;
+
+        return height_total;
+    }
+
+    @Override
+    void drawChilds(Canvas canvas, int x, int y, int height, int width) {
+        int x_child_offset = x + margin.left + padding.left;
+
+        for (LWidget child : childs) {
+            x_child_offset += child.margin.left + child.padding.left;
+
+            child.draw(canvas, x_child_offset, y, height, width);
+
+            x_child_offset += child.getWidth() + child.margin.right + child.margin.right;
+        }
     }
 }
