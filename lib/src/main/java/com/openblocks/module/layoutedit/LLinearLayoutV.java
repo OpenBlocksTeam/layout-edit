7c2a362a60b87f68323880f210efc760d6c1846a
Iyxan23 <nurihsanalghifari@gmail.com>
2021-03-16 16:08:06 +0700

    add: Draw gray bounds

--- lib/src/main/java/com/openblocks/module/layoutedit/LLinearLayoutV.java
+++ lib/src/main/java/com/openblocks/module/layoutedit/LLinearLayoutV.java
@@ -1,20 +1,55 @@
 package com.openblocks.module.layoutedit;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.util.AttributeSet;
 import android.view.DragEvent;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.LinearLayout;
+
+import androidx.annotation.Nullable;
 
 public class LLinearLayoutV extends LinearLayout {
 
     View shadow;
 
+    Paint outline_paint;
+
     public LLinearLayoutV(Context context) {
         super(context);
+
+        init(context);
+    }
+
+    public LLinearLayoutV(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+
+        init(context);
+    }
+
+    public LLinearLayoutV(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+
+        init(context);
+    }
+
+    public LLinearLayoutV(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+
+        init(context);
+    }
+
+    private void init(Context context) {
         setOrientation(VERTICAL);
 
         shadow = LayoutInflater.from(context).inflate(R.layout.shadow, null);
+
+        outline_paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        outline_paint.setStyle(Paint.Style.STROKE);
+        outline_paint.setColor(0xFF000000);
     }
 
     @Override
@@ -29,6 +64,7 @@ public class LLinearLayoutV extends LinearLayout {
 
                 LLinearLayoutV lLinearLayoutV = new LLinearLayoutV(getContext());
                 lLinearLayoutV.setPadding(8, 8, 8, 8);
+                lLinearLayoutV.setLayoutParams(new LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
                 addView(lLinearLayoutV);
 
                 break;
@@ -39,5 +75,12 @@ public class LLinearLayoutV extends LinearLayout {
         }
 
         return true;
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        canvas.drawRect(0, 0, getWidth(), getHeight(), outline_paint);
     }
 }
